0e1db5f0d647fc750a31177dcc3b011e71286b86
yuring2 <yuyu409300@naver.com>
2023-03-08 21:50:29 +0900

    commit

---
+++ src/프로그래머스/고득점kit/완전탐색/모음사전_2.java
@@ -0,0 +1,28 @@
+package 프로그래머스.고득점kit.완전탐색;
+
+import java.util.*;
+class 모음사전2 {
+    static String[] vowel = {"A","E","I","O","U"};
+    static int answer;
+    static int cnt = 0;
+    public static void dfs(String answer_word, String start){
+        if(start.equals(answer_word) || start.length()==5){
+            if(start.equals(answer_word)){
+                answer = cnt;
+            }
+            return;
+        }
+        for(int i=0; i<5; i++){
+            cnt++;
+            dfs(answer_word, start+vowel[i]);
+        }
+    } // end of dfs
+    public int solution(String word) {
+        dfs(word,"");
+        return answer;
+    }
+
+
+
+}
+
