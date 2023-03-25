c1b2ce2d0f6208d0e14274903c316aaaec4cc785
yuring2 <yuyu409300@naver.com>
2023-03-25 20:48:15 +0900

    commit

---
+++ src/백준/DFS_AND_BFS/N과M_3_복습.java
@@ -0,0 +1,33 @@
+package 백준.DFS_AND_BFS;
+
+import java.util.Scanner;
+
+public class N과M_3_복습 {
+    private static int n, m;
+    private static int[] arr;
+    private static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        arr = new int[m];
+        dfs(0);
+        System.out.println(sb);
+    } // end of main
+
+    public static void dfs(int d){
+        if(d==m) {
+            for (int k : arr){
+                sb.append(k + " ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for(int i=1; i<=n; i++){
+            arr[d] = i;
+            dfs(d+1);
+        }
+    } // end of dfs
+}
