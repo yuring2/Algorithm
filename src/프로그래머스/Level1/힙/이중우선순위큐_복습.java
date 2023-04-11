6dff585c936bf843ad95ec32dc65762d5820a2ab
yuring2 <yuyu409300@naver.com>
2023-04-11 23:17:01 +0900

    commit

---
+++ src/프로그래머스/Level1/힙/이중우선순위큐_복습.java
@@ -0,0 +1,39 @@
+package 프로그래머스.Level1.힙;
+import java.util.*;
+class 이중우선순위큐_복습 {
+    public int[] solution(String[] operations) {
+        int[] answer = new int[2];
+        PriorityQueue<Integer> minQ = new PriorityQueue<>();
+        PriorityQueue<Integer> maxQ = new PriorityQueue<>(Collections.reverseOrder());
+
+        for (String op : operations) {
+            StringTokenizer st = new StringTokenizer(op);
+            String key = st.nextToken();
+            int val = Integer.parseInt(st.nextToken());
+
+            if(minQ.size()==0 && key.equals("D")) continue;
+
+            if (key.equals("I")) {
+                minQ.offer(val);
+                maxQ.offer(val);
+                continue;
+            }
+
+            if(val < 0) {
+                int min = minQ.poll();
+                maxQ.remove(min);
+                continue;
+            }else{
+                int max = maxQ.poll();
+                minQ.remove(max);
+            }
+        }
+
+        if(minQ.size() > 0 ) {
+            answer[0] = maxQ.poll();
+            answer[1] = minQ.poll();
+        }
+
+        return answer;
+    } // end of solution()
+}
