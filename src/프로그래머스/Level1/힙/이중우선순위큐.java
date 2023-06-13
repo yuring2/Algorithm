f56a535790d00ac559d23e635293fdbc896d7874
yuring2 <yuyu409300@naver.com>
2023-06-13 23:34:54 +0900

    commit

--- src/프로그래머스/Level1/힙/이중우선순위큐.java
+++ src/프로그래머스/Level1/힙/이중우선순위큐.java
@@ -3,9 +3,8 @@ package 프로그래머스.Level1.힙;
 import java.util.*;
 class 이중우선순위큐 {
     public int[] solution(String[] operations) {
-        //String[] operations = {"I 16", "I -5643", "D -1", "D 1", "D 1", "I 123", "D -1"};
-        int[] answer = new int[2];
-        //최소 힙, 최대 힙
+         int[] answer = new int[2];
+
         PriorityQueue<Integer> minQ = new PriorityQueue<>();
         PriorityQueue<Integer> maxQ = new PriorityQueue<>(Collections.reverseOrder());
 
@@ -32,10 +31,11 @@ class 이중우선순위큐 {
             }
         }
 
-        if(minQ.size() > 0 ) { // 만약 큐에 아무 원소도 들어있지 않았다면 배열에는 0 0 이 들어있음.
+        if(minQ.size() > 0 ) {
             answer[0] = maxQ.poll();
             answer[1] = minQ.poll();
         }
+
         return answer;
     } // end of solution()
 }
